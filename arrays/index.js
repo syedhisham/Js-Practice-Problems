a33f863f8a784349ba075b93a246d1da4a3eb82d
Syed Hisham Shah <syedhishamshah27@gmail.com>
2025-01-20 00:43:42 +0500

    add: certain array methods

--- arrays/index.js
+++ arrays/index.js
@@ -1,5 +1,5 @@
 let max = 0;
-const fruitsArray = ["apple", "banana", "juice"];
+const fruitsArray = ["apple", "banana", "cherry"];
 console.log(typeof Array.isArray(fruitsArray));
 console.log("This is the first element of the array", fruitsArray[0]);
 for (let i = 0; i < fruitsArray.length; i++) {
@@ -14,3 +14,36 @@ fruitsArray.push('Peach')
 console.log("Array after pushing", fruitsArray);
 
 // Step 2: Array Methods
+const fruitsLength = fruitsArray.map((fruit) => {
+  return fruit.length
+})
+console.log(fruitsLength);
+const fruitsLengthWithMethod2 = fruitsArray.map(fruit => fruit.length)
+console.log(fruitsLengthWithMethod2);
+
+const filterArray = ['firstName', 'last', 'num']
+const filterArrayLength = filterArray.map(element => element.length)
+for(let index = 0; index < filterArrayLength.length; index++){
+  if (filterArrayLength[index] > 6) {
+    console.log('Only 6 characters are allowed. Correct the element with index: ' + index + ' and element: ' + filterArray[index]);
+  }
+}
+console.log(filterArrayLength);
+
+const newArrayWithLongFruitsNames = fruitsArray.filter(fruit => fruit.length > 5)
+console.log(newArrayWithLongFruitsNames);
+
+fruitsArray.forEach((element) => {
+  console.log(element);
+})
+
+
+
+
+
+
+
+
+ 
+
+ 
