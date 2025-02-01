9869baf70a1e2a36d84cf875e35a49f687f79d97
Syed Hisham Shah <syedhishamshah27@gmail.com>
2025-02-01 23:30:01 +0500

    add: 10_Declaring_Variables

---
+++ 10_Declaring_Variables/index.js
@@ -0,0 +1,56 @@
+// const foo = {bar: 10, baz: 20};
+// const {baz} = foo
+
+// console.log(baz);
+
+
+// // Variable scope
+
+// if (true) {
+//     var x = 5;
+//   }
+//   console.log(x);
+
+
+  // Understanding Variable Hoisting
+
+  // console.log(x === undefined);
+  // var x = 3;
+
+// const obj = {name : 'hisham'}
+//   obj.name = 'Alyan'
+//   console.log(obj);
+
+
+// Data type conversion
+
+
+// Numbers and the '+' operator
+// x = "10" +  42;
+// console.log(typeof x, + x);
+
+// Numbers and all other operators
+// let y = "20" - 10;
+// console.log(y, typeof y);
+
+// let z = "20" * 10;
+// console.log(z, typeof z);
+
+
+// Base Conversion
+// console.log(parseInt('101' , 2));
+
+// console.log(parseInt('101', 2));  // 5    (Binary → Decimal)
+// console.log(parseInt('101', 8));  // 65   (Octal → Decimal)
+// console.log(parseInt('101', 16)); // 257  (Hexadecimal → Decimal)
+
+
+// Converting strings to numbers
+console.log("1.1" + "1.11");
+console.log(+"1.1" + +"1.11");
+
+
+
+
+
+  
