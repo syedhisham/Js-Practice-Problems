53c48a847798d82e1ee33e68b85a128b2f59e89c
Syed Hisham Shah <syedhishamshah27@gmail.com>
2025-02-04 11:50:08 +0500

    add: 12_Conditional_Statements

---
+++ 12_Conditional_Statements/index.js
@@ -0,0 +1,69 @@
+// Falsy values
+// The following values evaluate to false (also known as Falsy values):
+
+// false
+// undefined
+// null
+// 0
+// NaN
+// the empty string ("")
+// All other values—including all objects—evaluate to true when passed to a conditional statement.
+
+
+// if(false){
+//     console.log("It is false");
+// }
+// else{
+//     console.log('It is true');
+// }
+
+// const b = new Boolean(false)
+// console.log(typeof b);
+
+// if(b){
+//     console.log('The false value is being printed');
+// }
+
+// const fruitType = 'Apples'
+
+// switch (fruitType) {
+//     case "Oranges":
+//       console.log("Oranges are $0.59 a pound.");
+//       break;
+//     case "Apples":
+//       console.log("Apples are $0.32 a pound.");
+//       break;
+//     case "Bananas":
+//       console.log("Bananas are $0.48 a pound.");
+//       break;
+//     case "Cherries":
+//       console.log("Cherries are $3.00 a pound.");
+//       break;
+//     case "Mangoes":
+//       console.log("Mangoes are $0.56 a pound.");
+//       break;
+//     case "Papayas":
+//       console.log("Papayas are $2.79 a pound.");
+//       break;
+//     default:
+//       console.log(`Sorry, we are out of ${fruitType}.`);
+//   }
+//   console.log("Is there anything else you'd like?");
+  
+// const color = 'blue'
+
+// switch(color){
+//     case 'yellow':
+//         console.log('You chose yellow color');
+//         break;
+//     case 'green':
+//         console.log('You chose green color');
+//         break;
+//     case 'red':
+//         console.log('You chose red color');
+//         break;
+//     default:
+//         console.log(`Sorry the color ${color} is not present in the list`)
+// }
+
+// console.log("Is there any color you want to add as a feedback?");
